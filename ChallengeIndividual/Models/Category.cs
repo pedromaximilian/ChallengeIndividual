5ebcd443825289c229944bdcdfd17934c624288c
pedromaximilian <pedro.maximilian@gmail.com>
2021-06-15 13:02:01 -0300

    Added Models

---
+++ ChallengeIndividual/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ChallengeIndividual.Models
+{
+    public partial class Category
+    {
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+    }
+}
---
+++ ChallengeIndividual/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ChallengeIndividual.Models
+{
+    public partial class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Article { get; set; }
+        public string Image { get; set; }
+        public int? CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
