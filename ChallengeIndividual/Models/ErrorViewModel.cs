0f7094770a07157ac36e2e856c88c6051be8ae08
pedromaximilian <pedro.maximilian@gmail.com>
2021-06-15 12:20:32 -0300

    Agregar archivos de proyecto.

---
+++ ChallengeIndividual/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChallengeIndividual.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
